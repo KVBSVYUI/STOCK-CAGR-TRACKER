4bbb474c8109e1661161ef39d788591b7d9e56fd
KVBSVYUI <mohitbhagat7971@gmail.com>
2026-09-09 22:02:32 +0530

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,11 +1,11 @@
 window.APP_CONFIG = {
   firebase: {
-    apiKey: "PASTE_FIREBASE_API_KEY",
-    authDomain: "PASTE_PROJECT.firebaseapp.com",
-    projectId: "PASTE_PROJECT_ID",
-    storageBucket: "PASTE_PROJECT.firebasestorage.app",
-    messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-    appId: "PASTE_APP_ID"
+    apiKey: "AIzaSyAokMEP3H618OgHAMLSoQcbFVE_DtPAiig",
+    authDomain: "booked-profit-tracker.firebaseapp.com",
+    projectId: "booked-profit-tracker",
+    storageBucket: "booked-profit-tracker.firebasestorage.app",
+    messagingSenderId: "401080852754",
+    appId: "1:401080852754:web:8f449ad7d2cd44314993cb"
   },
-  logoDevToken: "PASTE_LOGO_DEV_PUBLISHABLE_KEY"
+  logoDevToken: ""
 };
