6f933ff1cd07df91f6a2d1177fe7a3e760e6c41f
KVBSVYUI <mohitbhagat7971@gmail.com>
2026-09-10 04:02:17 +0530

    Correct Firebase config

--- app-config.js
+++ app-config.js
@@ -4,7 +4,7 @@ window.APP_CONFIG = {
     authDomain: "booked-profit-tracker.firebaseapp.com",
     projectId: "booked-profit-tracker",
     storageBucket: "booked-profit-tracker.firebasestorage.app",
-    messagingSenderId: "401080852852",
+    messagingSenderId: "401080852754",
     appId: "1:401080852754:web:8f449ad7d2cd44314993cb"
   },
   logoDevToken: ""
